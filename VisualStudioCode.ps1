6e9c31070ae721343a0b70cf1e754609b94c3b61
seros <github@seros.me>
2026-01-16 10:01:49 +0800

    feat: 更新

--- VisualStudioCode.ps1
+++ VisualStudioCode.ps1
@@ -2,10 +2,10 @@ Set-Location -Path $PSScriptRoot
 $UserEnter = Read-Host "Enter 1 to export, 2 to import"
 $SettingPath = "C:\Users\User\scoop\persist\vscode\data\user-data\User\settings.json"
 if ($UserEnter -eq "1") {
-    Copy-Item -Path $SettingPath
+    Copy-Item -Path $SettingPath -Destination .\.vscode
     code --list-extensions > extensions.txt
 }
 elseif ($UserEnter -eq "2") {
-    Copy-Item -Path settings.json -Destination $SettingPath
+    Copy-Item -Path .\.vscode\settings.json -Destination $SettingPath
     Get-Content extensions.txt | ForEach-Object { code --install-extension $_ }
 }
